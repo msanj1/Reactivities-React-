461373e8e6e2aaf988f2d8a314d5d548df50b019
Mohsen Sanjari <mohsen.sanjari@outlook.com>
2025-03-03 07:51:47 +1000

    Crated a skeleton project that integrates a React app with a .NET BE

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Domain.Activity> Activities { get; set; }
+    }
+}
---
+++ Persistence/DataContextFactory.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Persistence
+{
+    public class DataContextFactory : IDesignTimeDbContextFactory<DataContext>
+    {
+        public DataContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<DataContext>();
+            optionsBuilder.UseSqlite("Data Source=reactivities.db");
+
+            return new DataContext(optionsBuilder.Options);
+        }
+    }
+}
